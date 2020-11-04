61ef54029cce80e72b8dfb3d6aa10dc6a501be20
Ibrahim <adeniyiib360@gmail.com>
2020-11-04 16:35:02 +0100

    refactor: trim code

--- src/bot.js
+++ src/bot.js
@@ -22,8 +22,8 @@ export default class WhatsappBot {
     static async googleSearch(req, res, next) {
     
       const twiml = new MessagingResponse();
-      const searchParam = req.body.Body;
-      const searchParamArr = searchParam.split(" ")
+      const { Body: searchParam } = req.body;
+      const searchParamArr = searchParam? searchParam.split(" ") : []
       let rawStocksData = fs.readFileSync('src/stocks.json')
 
   
@@ -71,69 +71,10 @@ export default class WhatsappBot {
                     messageToSend = `${item.snippet} ${item.link} \n`
                     twiml.message(messageToSend)
                 })
-            
-                // twiml.message(messageToSend);
-        
+                    
                 res.set('Content-Type', 'text/xml');
             
-                // return res.status(200).send({ data: allResult})
                 return res.status(200).send(twiml.toString());
-            
-
-        // if (searchParamArr[0] === "#save") {
-        //     searchParamArr.shift()
-        //     let stocksData = JSON.parse(rawStocksData)
-        //     let newParamString = searchParamArr.join(" ")
-        //     stocksData.stocks.push(newParamString)
-        //     // console.log(stocksData, searchParamArr)
-
-        //     let stringifiedStocksData = JSON.stringify(stocksData, null, 2)
-        //     fs.writeFileSync('src/stocks.json', stringifiedStocksData)
-        //     twiml.message(`You have added ${newParamString} to the list. \nNew list: ${stocksData.stocks.join(", ")}`)
-
-        //     res.set('Content-Type', 'text/xml');
-        //     return res.status(200).send(twiml.toString());
-        // }
-
-        // if (searchParamArr[0] === "#list") {
-        //     let stocksData = JSON.parse(rawStocksData)
-        //     twiml.message(`The stocks you're watching are ${stocksData.stocks.join(", ")}`)    
-        //     res.set('Content-Type', 'text/xml');
-        //     return res.status(200).send(twiml.toString());        
-        // }
-
-        // if (searchParamArr[0] === "#menu") {
-        //     twiml.message(
-        //         `Features still limited. PRs acceptable! \n#menu: To get the menu list. 
-        //         \n#search 'phrase': to get top five results of the search. \n#list: To get list of stocks you're watching. 
-        //         \n#save 'stock': To add a stock to your list. \nBonus: You get news about your stocks daily.`
-        //     )
-        //     res.set('Content-Type', 'text/xml');
-        //     return res.status(200).send(twiml.toString());
-        // }
-
-        // Search Function
-        // if (searchParamArr[0] === '#search') {
-        //     searchParamArr.shift()
-        //     let newParamString = searchParamArr.join(" ")
-        //     const options = { cx: GOOGLECX, q: newParamString, auth: GOOGLEAPIKEY };
-        //     const result = await customsearch.cse.list(options);
-        //     // console.log(result.data.items)
-        //     const allResult = result.data.items;
-        //     let messageToSend = ""
-
-        //     allResult.slice(0, 5).forEach((item) => {
-        //         messageToSend = `${item.snippet} ${item.link} \n`
-        //         twiml.message(messageToSend)
-        //     })
-        
-        //     // twiml.message(messageToSend);
-    
-        //     res.set('Content-Type', 'text/xml');
-        
-        //     // return res.status(200).send({ data: allResult})
-        //     return res.status(200).send(twiml.toString());
-        // }
         }
       } catch (error) {
         return next(error);
